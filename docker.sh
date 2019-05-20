4b4277bed9c5e890aed734bd66f8e0f4970f6c31
hiephm <hiephm@gmail.com>
2019-05-20 18:27:19 +0700

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,16 +1,20 @@
 #!/usr/bin/env bash
 apt-get update
+
 apt-get install -y \
     apt-transport-https \
     ca-certificates \
     curl \
-    gnupg2 \
+    gnupg-agent \
     software-properties-common
-curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
+    
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+
 add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/debian \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
    
 apt-get update
+
 apt-get install -y docker-ce docker-ce-cli containerd.io
