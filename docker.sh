1466778c0af6f37ed83c5c551243b6e3da45d187
hiephm <hiephm@gmail.com>
2019-06-21 10:38:01 +0700

    Make script work on both Ubuntu and CentOS

--- docker.sh
+++ docker.sh
@@ -1,24 +1,36 @@
 #!/usr/bin/env bash
 ## Install Docker CE
-apt-get update
+distro="$(cat /etc/*release | head -n 1 | awk '{print $1}')"
 
-apt-get install -y \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    gnupg-agent \
-    software-properties-common
+if [ "$distro" == "CentOS" ]; then
+    yum install -y yum-utils device-mapper-persistent-data lvm2
     
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+    sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+    
+    yum install -y docker-ce docker-ce-cli containerd.io
+else
+    apt-get update
 
-add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
-   
-apt-get update
+    apt-get install -y \
+        apt-transport-https \
+        ca-certificates \
+        curl \
+        gnupg-agent \
+        software-properties-common
+        
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 
-apt-get install -y docker-ce docker-ce-cli containerd.io
+    add-apt-repository \
+       "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+       $(lsb_release -cs) \
+       stable"
+       
+    apt-get update
+
+    apt-get install -y docker-ce docker-ce-cli containerd.io
+fi
+
+
 
 ## Install Docker Compose
 curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
