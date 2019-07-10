d05c2878b8442a76351c1c6e67136c5af72011f5
hiephm <hiephm@gmail.com>
2019-07-10 15:38:08 +0700

    Create k8stool.sh

---
+++ k8stool.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+### Install kube-ps1
+curl -o ~/kube-ps1.sh https://raw.githubusercontent.com/jonmosco/kube-ps1/master/kube-ps1.sh
+
+echo source ~/kube-ps1.sh >> ~/.bashrc
+echo KUBE_PS1_SYMBOL_ENABLE=false >> ~/.bashrc
+echo PS1=\''[\u@\h \W $(kube_ps1)]\$ '\' >> ~/.bashrc
+
+### Install kubectx
+mkdir ~/kubectx
+curl -o ~/kubectx/kubectx https://raw.githubusercontent.com/ahmetb/kubectx/master/kubectx
+curl -o ~/kubectx/kubens https://raw.githubusercontent.com/ahmetb/kubectx/master/kubens
+chmod +x ~/kubectx/*
+echo 'export PATH=$PATH:~/kubectx' >> ~/.bashrc
