a7c20269dea0dea5f667f01c204d483e4724ff8e
hiephm <hiephm@gmail.com>
2019-07-23 13:30:58 +0700

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,38 +1,39 @@
 #!/usr/bin/env bash
+
 ## Install Docker CE
 distro="$(cat /etc/*release | head -n 1 | awk '{print $1}')"
 
 if [ "$distro" == "CentOS" ]; then
-    yum install -y yum-utils device-mapper-persistent-data lvm2
+    sudo yum install -y yum-utils device-mapper-persistent-data lvm2
     
     sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
     
-    yum install -y docker-ce docker-ce-cli containerd.io
+    sudo yum install -y docker-ce docker-ce-cli containerd.io
 else
-    apt-get update
+    sudo apt-get update
 
-    apt-get install -y \
+    sudo apt-get install -y \
         apt-transport-https \
         ca-certificates \
         curl \
         gnupg-agent \
         software-properties-common
         
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
-    add-apt-repository \
+    sudo add-apt-repository \
        "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
        $(lsb_release -cs) \
        stable"
        
-    apt-get update
+    sudo apt-get update
 
-    apt-get install -y docker-ce docker-ce-cli containerd.io
+    sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 fi
 
 
 
 ## Install Docker Compose
-curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 
-chmod +x /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
