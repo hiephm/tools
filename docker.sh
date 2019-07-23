8ea3ab8a4f9aa4126ec9957f6292b1c9459ceb68
hiephm <hiephm@gmail.com>
2019-07-23 16:03:12 +0700

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -9,6 +9,7 @@ if [ "$distro" == "CentOS" ]; then
     sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
     
     sudo yum install -y docker-ce docker-ce-cli containerd.io
+    sudo systemctl enable docker && sudo systemctl start docker
 else
     sudo apt-get update
 
