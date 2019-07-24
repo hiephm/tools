249593a4ab85419d5e9d95a0da3b2e225c7f8778
hiephm <hiephm@gmail.com>
2019-07-24 15:15:59 +0700

    Create wrk.sh

---
+++ wrk.sh
@@ -0,0 +1,10 @@
+#! /usr/bin/env bash
+
+# Installing wrk
+
+curl -L -o /tmp/wrk.zip https://github.com/giltene/wrk2/archive/master.zip
+unzip /tmp/wrk.zip -d /tmp
+cd /tmp/wrk2-master
+make -j 4
+sudo cp wrk /usr/bin/
+rm -rf /tmp/wrk.zip /tmp/wrk2-master
