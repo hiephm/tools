95fd123805d9661a8dd7661368bc7c0dcae552af
hiephm <hiephm@gmail.com>
2025-02-15 21:33:50 +0700

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -35,6 +35,6 @@ fi
 
 
 ## Install Docker Compose
-sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/v2.33.0/docker-compose-linux-x86_64" -o /usr/bin/docker-compose
 
-sudo chmod +x /usr/local/bin/docker-compose
+sudo chmod +x /usr/bin/docker-compose
